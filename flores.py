f596324fbf2d21c11357d7e25bae8277d5904304
NathQuintero <141141023+NathQuintero@users.noreply.github.com>
2024-05-30 20:30:36 -0500

    ok

--- flores.py
+++ flores.py
@@ -107,9 +107,6 @@ class_names = open("./clases (1).txt", "r").readlines()
 # Opción para capturar una imagen desde la cámara
 img_file_buffer = st.camera_input("Capture una foto para identificar el producto")
 
-# Opción para cargar una imagen desde un archivo local
-if img_file_buffer is None:
-    img_file_buffer = st.file_uploader("Cargar imagen desde archivo", type=["jpg", "jpeg", "png"])
 
 # Opción para cargar una imagen desde una URL
 if img_file_buffer is None:
