8b2d5670b572f1929ae9abf9cca64d2fd5cf07a8
NathQuintero <141141023+NathQuintero@users.noreply.github.com>
2024-05-31 07:56:01 -0500

    Update flores.py

--- flores.py
+++ flores.py
@@ -167,22 +167,21 @@ option = st.selectbox(
 
 img_file_buffer = None
 
-# Reproducir el saludo al inicio
-mp3_tp = generar_tipo()
-
-
 if option == "Tomar foto":
     # Opción para capturar una imagen desde la cámara
+    mp3_tp = generar_tipo()
     reproducir_audio(mp3_tp)
     img_file_buffer = st.camera_input("Capture una foto para identificar el producto")
 
 elif option == "Subir archivo":
+    mp3_tp = generar_tipo()
     reproducir_audio(mp3_tp)
     # Opción para cargar una imagen desde un archivo local
     if img_file_buffer is None:
         img_file_buffer = st.file_uploader("Cargar imagen desde archivo", type=["jpg", "jpeg", "png"])
 
 elif option == "URL":
+    mp3_tp = generar_tipo()
     reproducir_audio(mp3_tp)
     # Opción para cargar una imagen desde una URL
     if img_file_buffer is None:
